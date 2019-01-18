fd77d9385d46ca46c076065fa5514f681ffc2d3b
Dev <dorian.denicola@siicanada.com>
2019-01-18 16:13:36 -0500

    Add testing http mock for article service Add handleError for http requests

--- src/app/common/service/article.service.ts
+++ src/app/common/service/article.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Article } from '../model/article.model';
-import { Observable, of } from 'rxjs';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
 
 
 const endpoint = 'http://localhost:8080/article-api';
@@ -18,16 +19,36 @@ export class ArticleService {
 
 
   getAllArticles(): Observable<Article[]> {
-    return this.http.get<Article[]>(`${endpoint}/articles`);
+    return this.http.get<Article[]>(`${endpoint}/articles`).pipe(catchError(this.handleError('getAticles', [])));
   }
 
   getArticleById(id: number): Observable<Article> {
-    return this.http.get<Article>(`${endpoint}/articles/${id}`);
+    return this.http.get<Article>(`${endpoint}/articles/${id}`).pipe(catchError(this.handleError('getAticles', new Article())));
   }
 
   getArticlesByTag(tag: string): Observable<Article[]> {
-    return this.http.get<Article[]>(`${endpoint}/tags/${tag}/articles`);
+    return this.http.get<Article[]>(`${endpoint}/tags/${tag}/articles`).pipe(catchError(this.handleError('getAticles', [])));
   }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+  */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+ 
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+ 
+      // TODO: better job of transforming error for user consumption
+ 
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+ 
 }
 
 
