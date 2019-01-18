d8952832ab85254a22c76ed35612654c9856278b
Dev <dorian.denicola@siicanada.com>
2019-01-18 16:13:36 -0500

    Add testing http mock for article service Add handleError for http requests

--- src/app/common/service/article.service.spec.ts
+++ src/app/common/service/article.service.spec.ts
@@ -1,39 +1,55 @@
-import { async, TestBed } from "@angular/core/testing";
+import { TestBed } from "@angular/core/testing";
 
 import { ArticleService } from "./article.service";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 
 describe("ArticleService", () => {
   let service: ArticleService;
-  beforeEach(() => {
+  let httpMock: HttpTestingController;
+  beforeEach(async() => {
     TestBed.configureTestingModule({
-      providers: [ArticleService]
+        imports: [HttpClientTestingModule],
+        providers: [ArticleService]
     });
 
     service = TestBed.get(ArticleService);
+    httpMock = TestBed.get(HttpTestingController);
   });
 
   it("should be created", () => {
     expect(service).toBeTruthy();
   });
 
-  it("getAllArticles should return 6 articles", (done: DoneFn) => {
+  it("getAllArticles should return 6 articles", () => {
     service.getAllArticles().subscribe(value => {
       expect(value.length).toEqual(6);
-      done();
     });
+
+    const req = httpMock.expectOne('http://localhost:8080/article-api/articles');
+    expect(req.request.method).toEqual('GET');
+
+    httpMock.verify();
   });
 
-  it("getArticleById should return 6 articles", (done: DoneFn) => {
+  it("getArticleById should return 6 articles", () => {
     service.getArticleById(3).subscribe(value => {
       expect(value.id).toEqual(3);
-      done();
     });
+
+    const req = httpMock.expectOne('http://localhost:8080/article-api/articles/3');
+    expect(req.request.method).toEqual('GET');
+
+    httpMock.verify();
   });
 
-  it("getArticlesByTag should return 6 articles", (done: DoneFn) => {
+  it("getArticlesByTag should return 6 articles", () => {
     service.getArticlesByTag('science').subscribe(value => {
       expect(value.length).toEqual(2);
-      done();
     });
+
+    const req = httpMock.expectOne('http://localhost:8080/article-api/tags/science/articles');
+    expect(req.request.method).toEqual('GET');
+
+    httpMock.verify();
   });
 });
